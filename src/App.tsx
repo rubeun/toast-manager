1c9d1f96ea0858e747eb2fc619477727b6eb7bd6
Rubeun Tan <github@rubeun.com>
2024-06-19 12:02:59 -0700

    add ToastCreator to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ToastMessage from './components/ToastMessage/ToastMessage';
+import ToastCreator from './components/ToastCreator/ToastCreator';
 
 const App = () => {
+  const [horizontalPosition, setHorizontalPosition] = useState("left");
+  const [verticalPosition, setVerticalPosition] = useState("top");
+  const [message, setMessage] = useState("Message");
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Toast Manager</h1>
-
+        <ToastCreator />
         <ToastMessage />
+
+
+
       </header>
     </div>
   );
