614d7556328af4b677504e4141d9fe0759e8c8a4
Rubeun Tan <github@rubeun.com>
2024-06-20 15:42:52 -0700

    add Message type, tempData

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,51 @@ import './App.css';
 import ToastMessage from './components/ToastMessage/ToastMessage';
 import ToastCreator from './components/ToastCreator/ToastCreator';
 
+export type Message = {
+  id: number;
+  message: string;
+  position: string;
+  type: string;
+};
+
+const tempData = [
+  {
+    id: 0,
+    message: "Top Left Message 1",
+    position: "top-left",
+    type: "success"
+  },
+  {
+    id: 1,
+    message: "Top Right Message",
+    position: "top-right",
+    type: "error"
+  },
+  {
+    id: 2,
+    message: "Bottom Left Message",
+    position: "bottom-left",
+    type: "warning"
+  },
+  {
+    id: 3,
+    message: "Bottom Right Message",
+    position: "bottom-right",
+    type: "info"
+  },
+  {
+    id: 4,
+    message: "Top Left Message 2",
+    position: "top-left",
+    type: "info"
+  },
+];
+
 const App = () => {
   const [horizontalPosition, setHorizontalPosition] = useState("left");
   const [verticalPosition, setVerticalPosition] = useState("top");
   const [message, setMessage] = useState("Message");
+  const [toastMessages, setToastMessages] = useState<Message[]>(tempData);
 
   return (
     <div className="App">
@@ -14,9 +55,6 @@ const App = () => {
         <h1>Toast Manager</h1>
         <ToastCreator />
         <ToastMessage />
-
-
-
       </header>
     </div>
   );
