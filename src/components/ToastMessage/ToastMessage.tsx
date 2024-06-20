899d26e97a3da5cea10e7df225727c8ca1028e30
Rubeun Tan <github@rubeun.com>
2024-06-20 15:49:13 -0700

    split toastMessages into 4 corners, add styles

--- src/components/ToastMessage/ToastMessage.tsx
+++ src/components/ToastMessage/ToastMessage.tsx
@@ -1,9 +1,54 @@
 import styles from "./ToastMessage.module.css";
+import type { Message } from "../../App";
 
-const ToastMessage = () => {
+
+const ToastMessage = ({ toastMessages }: {toastMessages: Message[]}) => {
+
+  const topLeftPositionedMessages = toastMessages.filter(
+    (msg) => msg.position === "top-left",
+  );
+  const topRightPositionedMessages = toastMessages.filter(
+    (msg) => msg.position === "top-right",
+  );
+  const bottomLeftPositionedMessages = toastMessages.filter(
+    (msg) => msg.position === "bottom-left",
+  );
+  const bottomRightPositionedMessages = toastMessages.filter(
+    (msg) => msg.position === "bottom-right",
+  );
+
 
   return (
-    <div className={styles.toastMessage}>Placeholder Message</div>
+    <div className={styles.toastMessage}>
+      <div className={`${styles.topLeft} ${styles.messages}`}>
+        {topLeftPositionedMessages.map((msg, idx) => (
+          <div className={styles.message} key={idx}>
+            {msg.message}
+          </div>
+        ))}
+      </div>
+      <div className={`${styles.topRight} ${styles.messages}`}>
+        {topRightPositionedMessages.map((msg, idx) => (
+          <div className={styles.message} key={idx}>
+            {msg.message}
+          </div>
+        ))}
+      </div>
+      <div className={`${styles.bottomLeft} ${styles.messages}`}>
+        {bottomLeftPositionedMessages.map((msg, idx) => (
+          <div className={styles.message} key={idx}>
+            {msg.message}
+          </div>
+        ))}
+      </div>
+      <div className={`${styles.bottomRight} ${styles.messages}`}>
+        {bottomRightPositionedMessages.map((msg, idx) => (
+          <div className={styles.message} key={idx}>
+            {msg.message}
+          </div>
+        ))}
+      </div>      
+    </div>
   );
 }
 
