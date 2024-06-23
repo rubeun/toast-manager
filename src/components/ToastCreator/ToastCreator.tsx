c15d4fc2ec72dac839043be21e34ddca06460847
Rubeun Tan <github@rubeun.com>
2024-06-22 23:08:32 -0700

    add position state management

--- src/components/ToastCreator/ToastCreator.tsx
+++ src/components/ToastCreator/ToastCreator.tsx
@@ -1,13 +1,30 @@
+import React, { useState } from "react";
 import styles from "./ToastCreator.module.css";
 
 const ToastCreator = () => {
+  const [horizontalPosition, setHorizontalPosition] = useState("left");
+  const [verticalPosition, setVerticalPosition] = useState("top");
+
+  const changeHorizontalPosition = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setHorizontalPosition(event.target.value);
+  };
+
+  const changeVerticalPosition = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setVerticalPosition(event.target.value);
+  };
+
+
   return (
     <div className={styles.toastCreator}>
-      <select>
+      <select onChange={changeHorizontalPosition} value={horizontalPosition}>
         <option value="left">Left</option>
         <option value="right">Right</option>
       </select>
-      <select>
+      <select onChange={changeVerticalPosition} value={verticalPosition}>
         <option value="top">Top</option>
         <option value="bottom">Bottom</option>
       </select>
