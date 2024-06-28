50529100316f51026c7e21da1304de87e0f03198
Rubeun Tan <github@rubeun.com>
2024-06-28 02:11:19 -0700

    swap out css module for css

--- src/components/ToastMessage/ToastMessage.tsx
+++ src/components/ToastMessage/ToastMessage.tsx
@@ -1,14 +1,14 @@
-import styles from "./ToastMessage.module.css";
+import "./ToastMessage.css";
 import type { Message } from "../../App";
 
 const ToastMessage = ({ toastMessages, closeToast }: {toastMessages: Message[], closeToast: any}) => {
 
   const messageDiv = (messageObj: Message, index: number) => {
     return (
-      <div className={`${styles[messageObj.type]} ${styles.message}`} key={index}>
+      <div className={`${[messageObj.type]} message`} key={index} id={messageObj.id.toString()}>
         {messageObj.message}
         <button
-          className={styles.messageButton}
+          className="messageButton"
           onClick={() => closeToast(messageObj.id)}
         >
           &#10006;
@@ -33,17 +33,17 @@ const ToastMessage = ({ toastMessages, closeToast }: {toastMessages: Message[], 
 
 
   return (
-    <div className={styles.toastMessage}>
-      <div className={`${styles.topLeft} ${styles.messages}`}>
+    <div className="toastMessage">
+      <div className="topLeft messages">
         {topLeftPositionedMessages.map((msg, idx) => messageDiv(msg, idx))}
       </div>
-      <div className={`${styles.topRight} ${styles.messages}`}>
+      <div className="topRight messages">
         {topRightPositionedMessages.map((msg, idx) => messageDiv(msg, idx))}
       </div>
-      <div className={`${styles.bottomLeft} ${styles.messages}`}>
+      <div className="bottomLeft messages">
         {bottomLeftPositionedMessages.map((msg, idx) => messageDiv(msg, idx))}
       </div>
-      <div className={`${styles.bottomRight} ${styles.messages}`}>
+      <div className="bottomRight messages">
         {bottomRightPositionedMessages.map((msg, idx) => messageDiv(msg, idx))}
       </div>      
     </div>
